d12f1f591fa7b3522ec35d36d31c9bc064f154b8
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 03:29:06 -0400

    Add files via upload

---
+++ og`abek 110/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+
+
+        if (a % 5 == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
